beb4d340433ed455b66b0dc43ef5278b0fc9f31e
Funcry241 <w.mair@aisol.de>
2025-07-23 20:10:51 +0200

    Auto-Commit: Changes OtterDream 2025-07-23 20:10:51

--- build.ps1
+++ build.ps1
@@ -128,7 +128,7 @@ if ($LASTEXITCODE -ne 0) {
 
 # Build
 Write-Host "[BUILD] Starting build..."
-cmake --build build --config $Configuration --parallel
+cmake --build build --config $Configuration --parallel -- -v
 if ($LASTEXITCODE -ne 0) {
     if (-not (Test-Path "build/build.ninja")) {
         Write-Error "[NINJA] build.ninja missing. CMake-Konfiguration war fehlerhaft."
