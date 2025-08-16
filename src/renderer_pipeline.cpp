caf4e252cd42648fd65107cf0264b3adfec58b0f
Funcry241 <w.mair@aisol.de>
2025-08-16 11:07:49 +0200

    Auto-Commit: Changes OtterDream 2025-08-16 11:07:49

--- src/renderer_pipeline.cpp
+++ src/renderer_pipeline.cpp
@@ -1,7 +1,7 @@
 // Datei: src/renderer_pipeline.cpp
 // 🐭 Maus-Kommentar: Kompakt, robust, Shader-Errors werden sauber erkannt. VAO-Handling und OpenGL-State sind clean - HUD/Heatmap bleiben garantiert sichtbar.
-// 🦦 Otter: Keine OpenGL-Misere, Schneefuchs freut sich über stabile Pipelines. (Bezug zu Otter)
-// 🐑 Schneefuchs: Fehlerquellen mit glGetError sichtbar gemacht, Upload deterministisch. (Bezug zu Schneefuchs)
+// 🦦 Otter: Keine OpenGL-Misere, Schneefuchs freut sich über stabile Pipelines.
+// 🐑 Schneefuchs: Fehlerquellen mit glGetError sichtbar gemacht, Upload deterministisch.
 
 #include "pch.hpp"
 #include "renderer_pipeline.hpp"
@@ -37,54 +37,45 @@ void init() {
         LUCHS_LOG_HOST("[FATAL] Shader program creation failed - aborting");
         std::exit(EXIT_FAILURE);
     }
-    if constexpr (Settings::debugLogging) {
-        LUCHS_LOG_HOST("[PIPELINE] Shader program created: %u", program);
-    }
-
+    LUCHS_LOG_HOST("[PIPELINE] Shader program created: %u", program);
+    
     glUseProgram(program);
     glUniform1i(glGetUniformLocation(program, "uTex"), 0);
     glUseProgram(0);
-    if constexpr (Settings::debugLogging) {
-        LUCHS_LOG_HOST("[PIPELINE] Uniform 'uTex' set to texture unit 0");
-    }
+    LUCHS_LOG_HOST("[PIPELINE] Uniform 'uTex' set to texture unit 0");
 
     OpenGLUtils::createFullscreenQuad(&VAO, &VBO, &EBO);
-    if constexpr (Settings::debugLogging) {
-        LUCHS_LOG_HOST("[PIPELINE] Fullscreen quad VAO=%u VBO=%u EBO=%u created", VAO, VBO, EBO);
-    }
+    LUCHS_LOG_HOST("[PIPELINE] Fullscreen quad VAO=%u VBO=%u EBO=%u created", VAO, VBO, EBO);
 }
 
 void updateTexture(GLuint pbo, GLuint tex, int width, int height) {
-    if constexpr (Settings::debugLogging) {
+    if (Settings::debugLogging) {
         LUCHS_LOG_HOST("[GL-UPLOAD] Binding PBO=%u and Texture=%u for upload", pbo, tex);
     }
 
     glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
     glBindTexture(GL_TEXTURE_2D, tex);
 
-    if constexpr (Settings::debugLogging) {
+    if (Settings::debugLogging) {
         LUCHS_LOG_HOST("[GL-UPLOAD] Calling glTexSubImage2D with dimensions %dx%d", width, height);
     }
 
     glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
 
-    if constexpr (Settings::debugLogging) {
-        GLenum err = glGetError();
+    GLenum err = glGetError();
+    if (Settings::debugLogging)
         LUCHS_LOG_HOST("[GL-UPLOAD] glTexSubImage2D glGetError() = 0x%04X", err);
-    }
 
     glBindTexture(GL_TEXTURE_2D, 0);
     glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
 
-    if constexpr (Settings::debugLogging) {
+    if (Settings::debugLogging)
         LUCHS_LOG_HOST("[GL-UPLOAD] Texture update from PBO complete");
-    }
 }
 
 void drawFullscreenQuad(GLuint tex) {
-    if constexpr (Settings::debugLogging) {
+    if (Settings::debugLogging)
         LUCHS_LOG_HOST("[DRAW] About to draw fullscreen quad with Texture=%u", tex);
-    }
 
     glUseProgram(program);
     glDisable(GL_DEPTH_TEST);
@@ -96,49 +87,25 @@ void drawFullscreenQuad(GLuint tex) {
 
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-    if constexpr (Settings::debugLogging) {
-        GLenum err = glGetError();
+    GLenum err = glGetError();
+    if (Settings::debugLogging)
         LUCHS_LOG_HOST("[DRAW] glDrawElements glGetError() = 0x%04X", err);
-    }
 
     glBindVertexArray(0);
     glUseProgram(0);
 
-    if constexpr (Settings::debugLogging) {
+    if (Settings::debugLogging)
         LUCHS_LOG_HOST("[DRAW] Fullscreen quad drawn");
-    }
 }
 
 void cleanup() {
-    if (program) {
-        glDeleteProgram(program);
-        if constexpr (Settings::debugLogging) {
-            LUCHS_LOG_HOST("[CLEANUP] Deleted program %u", program);
-        }
-    }
-    if (VAO) {
-        glDeleteVertexArrays(1, &VAO);
-        if constexpr (Settings::debugLogging) {
-            LUCHS_LOG_HOST("[CLEANUP] Deleted VAO %u", VAO);
-        }
-    }
-    if (VBO) {
-        glDeleteBuffers(1, &VBO);
-        if constexpr (Settings::debugLogging) {
-            LUCHS_LOG_HOST("[CLEANUP] Deleted VBO %u", VBO);
-        }
-    }
-    if (EBO) {
-        glDeleteBuffers(1, &EBO);
-        if constexpr (Settings::debugLogging) {
-            LUCHS_LOG_HOST("[CLEANUP] Deleted EBO %u", EBO);
-        }
-    }
+    if (program) { glDeleteProgram(program); LUCHS_LOG_HOST("[CLEANUP] Deleted program %u", program); }
+    if (VAO)    { glDeleteVertexArrays(1, &VAO); LUCHS_LOG_HOST("[CLEANUP] Deleted VAO %u", VAO); }
+    if (VBO)    { glDeleteBuffers(1, &VBO);      LUCHS_LOG_HOST("[CLEANUP] Deleted VBO %u", VBO); }
+    if (EBO)    { glDeleteBuffers(1, &EBO);      LUCHS_LOG_HOST("[CLEANUP] Deleted EBO %u", EBO); }
 
     program = VAO = VBO = EBO = 0;
-    if constexpr (Settings::debugLogging) {
-        LUCHS_LOG_HOST("[CLEANUP] RendererPipeline resources cleaned up");
-    }
+    LUCHS_LOG_HOST("[CLEANUP] RendererPipeline resources cleaned up");
 }
 
 } // namespace RendererPipeline
