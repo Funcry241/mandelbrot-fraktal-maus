7ea03043b63b55ea5a79f767c79995bcfd9072a0
Funcry241 <w.mair@aisol.de>
2025-09-30 18:26:00 +0200

    Auto-Commit: Changes OtterDream 2025-09-30 18:26:00

--- src/cuda_interop.cu
+++ src/cuda_interop.cu
@@ -168,8 +168,7 @@ void renderCudaFrame(
     // Silencing unreferenced-out-params (API bleibt stabil)
     (void)newOffsetX;
     (void)newOffsetY;
-    (void)shouldZoom;
-
+    (void)shouldZoom;      
     if (!s_pboActive) {
         LUCHS_LOG_HOST("[PBO][ERR] render called without registered PBOs");
         state.skipUploadThisFrame = true;
